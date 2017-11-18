d32ff0394bbce7fd6ce61ecf739ca8f67208b864
Aska <arslanannaev7@gmail.com>
2017-11-18 14:30:25 +0200

    трариз

---
+++ 5 semestr/ТРАРИЗ/ПО/LabaratorTRARIZ/Labarator_3_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labarator_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Рекурсивная версия алгоритма Хоара
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="begin"></param>
+        /// <param name="end"></param>
+        static void HoarSort(double[] data, int begin, int end)
+        {
+            int iterator = begin, j = end;
+            double temp;
+            double middle = data[(begin + end) / 2];
+
+            do
+            {
+                while (data[iterator] < middle)
+                    iterator++;
+
+                while (data[j] > middle)
+                    j--;
+
+                if (iterator <= j)
+                {
+                    if (iterator < j)
+                    {
+                        temp = data[iterator];
+                        data[iterator] = data[j];
+                        data[j] = temp;
+                    }
+                    iterator++;
+                    j--;
+                }
+            } while (iterator <= j);
+
+            if (iterator < end)
+                HoarSort(data, iterator, end);
+            if (begin < j)
+                HoarSort(data, begin, j);
+        }
+    }
+}
