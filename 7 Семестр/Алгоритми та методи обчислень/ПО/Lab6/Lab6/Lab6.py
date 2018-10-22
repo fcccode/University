c87e08f45b5a4bec8f443a4fe7822e7707fcbfa0
Aska2790 <arslanannaev7@gmail.com>
2018-10-22 13:59:35 +0300

    АМО Лабораторная №6 и №7 готовы

---
+++ 7 Семестр/Алгоритми та методи обчислень/ПО/Lab6/Lab6/Lab6.py
@@ -0,0 +1,24 @@
+
+from itertools import combinations 
+
+# Исходные данные
+n = 6;
+m = 4;
+
+# Генерация сочетаний по заданным критериям
+if n<m:
+    print("Заданы не корректные параметры поиска");
+    exit(0); 
+
+
+comb = combinations(range(1,n+1), m) 
+  
+# Отображение общее количество вариантов
+temp = list(comb)
+print("n = ",n);
+print("m = ",m);
+print("Общее количество сочетаний = ", len(temp));
+
+# Отображение сгенерированных комбинаций
+for i in temp: 
+    print(i) 
---
+++ 7 Семестр/Алгоритми та методи обчислень/ПО/Lab7/Lab7/Lab7.py
@@ -0,0 +1,66 @@
+import collections
+ 
+# Этот класс представляет собой ориентированный граф с использованием представления матрицы смежности
+class Graph:
+  
+    def __init__(self,graph):
+        self.graph = graph  # residual graph
+        self.ROW = len(graph)
+  
+    def BFS(self, s, t, parent):
+        '''Возвращает true, если есть путь от источника к истоку. 
+        Также заполняет родительский элемент [], чтобы сохранить путь '''
+
+        visited = [False] * (self.ROW)
+        queue = collections.deque()
+        queue.append(s)
+        visited[s] = True
+         
+        while queue:
+            u = queue.popleft()
+            for ind, val in enumerate(self.graph[u]):
+                if (visited[ind] == False) and (val > 0):
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+        return visited[t]
+             
+    # Возвращает максимальный поток от источника до истока в данном графе
+    def FordFulkerson(self, source, sink):
+        parent = [-1] * (self.ROW)
+        max_flow = 0 
+        while self.BFS(source, sink, parent):
+            path_flow = float("Inf")
+            s = sink
+            while s != source:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+            max_flow += path_flow
+            v = sink
+            while v != source:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+ 
+        return max_flow
+
+
+if __name__ == "__main__":
+   
+   adj_matrix = [[0, 16, 13, 0, 0, 0], 
+                 [0, 0, 10, 12, 0, 0], 
+                 [0, 4, 0,  0,  14,0], 
+                 [0, 0, 9,  0,  0, 20], 
+                 [0, 0, 0,  7,  0, 4], 
+                 [0, 0, 0,  0,  0, 0]] 
+
+   source = 0
+   sink = 5
+
+   graph = Graph(adj_matrix)
+   max_flow = graph.FordFulkerson(source, sink)
+  
+   print("Максимальная пропускная способность %d " % max_flow) 
+ 
+  
