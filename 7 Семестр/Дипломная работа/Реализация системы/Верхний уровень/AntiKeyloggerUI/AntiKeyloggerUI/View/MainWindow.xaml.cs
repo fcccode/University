a6dcd227a6881a794576fd28b12ed39774bfb8fa
Aska <annaev.arslan90@gmail.com>
2018-11-25 20:44:47 +0200

    1

---
+++ 7 Семестр/Дипломная работа/Реализация системы/Верхний уровень/AntiKeyloggerUI/AntiKeyloggerUI/View/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using AntiKeyloggerUI.Auxiliary;
+using AntiKeyloggerUI.Properties;
+using AntiKeyloggerUI.ViewModel;
+
+using System.ComponentModel;
+using System.Windows;
+
+namespace AntiKeyloggerUI.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UiDispatcherHelper.Initialize();
+            DataContext = new MainViewModel();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            Settings.Default.Save();
+            base.OnClosing(e);
+        }
+    }
+}
---
+++ 7 Семестр/Дипломная работа/Реализация системы/Верхний уровень/AntiKeyloggerUI/AntiKeyloggerUI/ViewModel/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using AntiKeyloggerUI.Auxiliary;
+using AntiKeyloggerUI.Models;
+using AntiKeyloggerUI.Properties;
+using System.Windows.Input;
+
+namespace AntiKeyloggerUI.ViewModel
+{
+    public class MainViewModel :BindingProperty
+    {
+        #region Команды
+
+        /// <summary>
+        /// Управления окном настройки
+        /// </summary>
+        private ICommand _cmdSetting;
+        public ICommand CmdSetting
+        {
+            get
+            {
+                if (_cmdSetting == null)
+                    _cmdSetting = new ActionCommand(() =>
+                    {
+                        SettingWidth = (SettingWidth == 0) ? double.NaN : 0;
+                        Settings.Default.SettingWidth = SettingWidth;
+                        Settings.Default.Save();
+                    });
+                return _cmdSetting;
+            }
+        }
+
+        #endregion
+
+        #region Переменные
+
+
+        #endregion
+
+        #region Свойства
+
+        /// <summary>
+        /// Значение управления шириной окна настройки
+        /// </summary>
+        private double _settingWidth;
+        public double SettingWidth
+        {
+            get { return _settingWidth; }
+            set
+            {
+                _settingWidth = value;
+                OnPropertyChanged(nameof(SettingWidth));
+            }
+        }
+
+       
+        /// <summary>
+        /// Модель отображения пользовательских уведомлений
+        /// </summary>
+        public ToastViewModel Notificate { get; private set; }
+
+        #endregion
+
+        #region Конструктор
+        public MainViewModel()
+        {
+            Notificate = new ToastViewModel();
+        }
+        #endregion
+
+        #region Методы
+      
+        #endregion
+
+        #region Обработчики команд
+     
+        #endregion
+       
+    }
+}
